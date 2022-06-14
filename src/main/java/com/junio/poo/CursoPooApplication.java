ba6b71de737d95252e328987699d15b7cbcf8c21
Junio Figueiredo <junioinf@gmail.com>
2022-06-14 17:02:24 -0300

    Projeto criado

---
+++ src/main/java/com/junio/poo/CursoPooApplication.java
@@ -0,0 +1,13 @@
+package com.junio.poo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoPooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoPooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/junio/poo/CursoPooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junio.poo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoPooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
